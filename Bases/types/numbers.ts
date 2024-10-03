167a1a2ffcda6d3eb4b947ab48eafa349155c922
Camilo López <91794023+CamiloLopez15@users.noreply.github.com>
2024-10-03 16:54:48 -0500

    feat: added class 16

---
+++ Bases/types/numbers.ts
@@ -0,0 +1,13 @@
+(() => {
+
+  let avengers: number;
+  const villains: number = 20;
+
+  avengers = Number("55")
+
+  if(avengers < villains) console.log("Estamos en problemas");
+  else console.log("Nos salvamos");
+
+  console.log({avengers});
+
+})()
