b35ce33409d2c4b10486b8b975f4f96a640e2101
Camilo López <91794023+CamiloLopez15@users.noreply.github.com>
2024-10-15 17:14:48 -0500

    feat: added class 85

--- ts-node/src/index.ts
+++ ts-node/src/index.ts
@@ -1,8 +1,2 @@
-import { Hero } from './classes/Hero';
-// import { Hero as SuperHero, Hero2 } from './classes/Hero';
-// import * as HeroClasses from './classes/Hero';
-// const ironman = new SuperHero('Ironman', 1, 55);
-const ironman = new Hero('Ironman', 10, 55);
+import { printObject } from "./generics/generics";
 
-console.log( ironman );
-console.log( ironman.power );
